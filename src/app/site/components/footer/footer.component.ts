ff40b2c1042bac9e6fdc2e49ea8e474ef20b1040
Ruan <ruansouza.co@gmail.com>
2025-07-03 11:40:16 -0300

    Alterações gerais

--- src/app/site/components/footer/footer.component.ts
+++ src/app/site/components/footer/footer.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router'; 
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'web-footer',
-  standalone: true, 
-  imports: [RouterModule], 
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
