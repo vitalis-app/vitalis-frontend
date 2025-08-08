1ff7b1d4b979161ee471add281a449ca6de05c7c
Vitor de Moraes <vitordemoraesfilho@gmail.com>
2025-08-08 13:36:21 -0300

    aplicação 40% restaurada
    faltam muitos detalhes

--- front-end/src/app/web-app/components/sidebar/sidebar.component.ts
+++ front-end/src/app/web-app/components/sidebar/sidebar.component.ts
@@ -1,72 +1,25 @@
-import { Component, OnInit, OnDestroy, Output, EventEmitter } from '@angular/core';
-import { ThemeService } from '../../../shared/services/theme.service';
-import { Subscription } from 'rxjs';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css'],
+  styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements OnInit, OnDestroy {
-  isCollapsed = false;
-  modoHover = false;
+export class SidebarComponent {
+  
+  isSidebarOpen = false;
 
-  @Output() collapsedChange = new EventEmitter<boolean>();
+  constructor() { }
 
-  public currentTheme: 'light' | 'dark' = 'light';
-  public logoSrc: string = '';
-  private themeSubscription!: Subscription;
-  constructor(private themeService: ThemeService) { }
-
-  ngOnInit(): void {
-    this.themeSubscription = this.themeService.theme$.subscribe(theme => {
-      this.currentTheme = theme;
-      this.updateLogo();
-    });
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
   }
 
-  ngOnDestroy(): void {
-    if (this.themeSubscription) {
-      this.themeSubscription.unsubscribe();
-    }
-  }
-  private updateLogo(): void {
-    const isDark = this.currentTheme === 'dark';
-    if (this.isCollapsed) {
-      this.logoSrc = isDark
-        ? 'assets/imgs/vitalis-icon-branco.png' // Tema escuro -> ícone branco
-        : 'assets/imgs/vitalis-icon.png';       // Tema claro -> ícone escuro
-    } else {
-      this.logoSrc = isDark
-        ? 'assets/imgs/vitalis-branca.png'      // Tema escuro -> logo branca
-        : 'assets/imgs/vitalis.png';            // Tema claro -> logo escura
-    }
-  }
-
-  handleMouseEnter(): void {
-    if (this.modoHover) {
-      this.isCollapsed = false;
-      this.collapsedChange.emit(this.isCollapsed);
-      this.updateLogo();
-    }
-  }
-
-  handleMouseLeave(): void {
-    if (this.modoHover) {
-      this.isCollapsed = true;
-      this.collapsedChange.emit(this.isCollapsed);
-      this.updateLogo();
-    }
-  }
-
-  toggleHoverMode() {
-    this.modoHover = !this.modoHover;
-    if (this.modoHover) {
-      this.isCollapsed = true;
-    } else {
-      this.isCollapsed = false;
-    }
-    this.updateLogo();
-    this.collapsedChange.emit(this.isCollapsed);
+  closeSidebar() {
+    this.isSidebarOpen = false;
   }
 }
